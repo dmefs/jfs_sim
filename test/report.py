cd300c4395218fd7f9485793c6ca17df88260b2b
dennis <dennis.mefs@gmail.com>
2021-05-10 17:26:34 +0800

    Auto generate Report

--- test/report.py
+++ test/report.py
@@ -1,9 +1,41 @@
+from io import DEFAULT_BUFFER_SIZE
 import pandas as pd
+import csv
+size_col = ["Read actual size","Read virtual size","Write actual size",
+            "Write virtual size","Delete write actual size",
+            "Delete write virtual size","Delete read virtual size"]
+time_col = ["Access time", "Write time", "Read time", "Read virtual time", "Delete write time", "Delete read time"]
+feature_name = ["Native", "Zalloc", "TopBuffer", "VG"]
 txt_size = 'bin/size.log'
 txt_time = 'bin/time.log'
-dsize = pd.read_csv(txt_size, sep=',')
-dtime = pd.read_csv(txt_time, sep=',')
-dsize.to_excel('bin/output.xlsx', index=False, sheet_name='size')
-with pd.ExcelWriter('bin/output.xlsx', engine='openpyxl', mode='a') as writer:
-    dtime.to_excel(writer, sheet_name='time')
-    writer.save()
+dsize = []
+dtime = []
+def getdf(sizeName, timeName):
+    size_list = []
+    time_list = []
+    with open(sizeName, 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            size_list.append(row)
+    with open(timeName, 'r') as fd:
+        reader = csv.reader(fd)
+        for row in reader:
+            time_list.append(row)
+    dsize = pd.DataFrame(size_list, index=feature_name, columns=size_col)
+    dtime = pd.DataFrame(time_list, index=feature_name, columns=time_col)
+    return (dsize, dtime)
+
+(df_size, df_time) = getdf("bin/50%size.log", "bin/50%time.log")
+dsize.append(df_size)
+dtime.append(df_time)
+(df_size, df_time) = getdf("bin/75%size.log", "bin/75%time.log")
+dsize.append(df_size)
+dtime.append(df_time)
+(df_size, df_time) = getdf("bin/90%size.log", "bin/90%time.log")
+dsize.append(df_size)
+dtime.append(df_time)
+percent = [50, 75, 90]
+with pd.ExcelWriter('bin/output.xlsx') as writer:
+    for i in range(3):
+        dsize[i].to_excel(writer, sheet_name="{0}%size".format(percent[i]))
+        dtime[i].to_excel(writer, sheet_name="{0}%time".format(percent[i]))
---
+++ test/report.sh
@@ -0,0 +1,22 @@
+# log 50% trace
+make native zalloc top_buffer vg
+JJFS=('jfs_native' 'jfs_zalloc' 'jfs_topbuffer' 'jfs_vg')
+rm ./bin/*.log
+for exe in "${JJFS[@]}"
+do
+    ./bin/$exe -i instructions/1m_1024_333 -s 2 -l
+done
+mv ./bin/size.log ./bin/50%size.log
+mv ./bin/time.log ./bin/50%time.log
+for exe in "${JJFS[@]}"
+do
+    ./bin/$exe -i instructions/1m_1536_333 -s 2 -l
+done
+mv ./bin/size.log ./bin/75%size.log
+mv ./bin/time.log ./bin/75%time.log
+for exe in "${JJFS[@]}"
+do
+    ./bin/$exe -i instructions/1m_1844_333 -s 2 -l
+done
+mv ./bin/size.log ./bin/90%size.log
+mv ./bin/time.log ./bin/90%time.log
